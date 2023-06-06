269aea2d4acbfaf521b77a6d969d7b30d7063902
jbijlsma <jeroen_work@yahoo.com>
2023-06-06 14:30:10 +0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  CBAnimatedNavBar,
+  CBAnimatedHeader,
+  CBAnimatedTabBar,
+  CBTabRoute,
+  CBTabView,
+  CBTabBar,
+} from "./lib";
+import { useScrollManager } from "./hooks";
+import { Scene, NavBar, NavBarTitle, Header } from "./components";
+
+const initialWidth = Dimensions.get("window").width;
+export type tabKeys = "all" | "tradable" | "gainers" | "losers";
+export const tabs = [
+  { key: "all" as tabKeys, title: "All assets" },
+  { key: "gainers" as tabKeys, title: "Gainers" },
+  { key: "losers" as tabKeys, title: "Losers" },
+];
 
 export default function App() {
+  const { scrollY, index, setIndex, getRefForKey, ...sceneProps } =
+    useScrollManager(tabs);
+
+  const renderScene = useCallback(
+    ({ route: tab }: { route: CBTabRoute }) => (
+      <Scene
+        isActive={tabs[index].key === tab.key}
+        routeKey={tab.key}
+        scrollY={scrollY}
+        {...sceneProps}
+      />
+    ),
+    [getRefForKey, index, tabs, scrollY]
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavBar>
+        <CBAnimatedNavBar scrollY={scrollY}>
+          <NavBarTitle />
+        </CBAnimatedNavBar>
+      </NavBar>
+
+      <View style={styles.screen}>
+        <CBAnimatedHeader scrollY={scrollY}>
+          <Header />
+        </CBAnimatedHeader>
+
+        <CBTabView
+          index={index}
+          setIndex={setIndex}
+          width={initialWidth}
+          routes={tabs}
+          renderTabBar={(p) => (
+            <CBAnimatedTabBar scrollY={scrollY}>
+              <CBTabBar {...p} />
+            </CBAnimatedTabBar>
+          )}
+          renderScene={renderScene}
+        />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+# Animated Tabbar
+
+As published by Coinbase in March 2021:
+
+https://www.coinbase.com/blog/coinbases-animated-tabbar-in-react-native
+
+Outdated Github repo:
+
+https://github.com/coinbase/CBTabViewExample
